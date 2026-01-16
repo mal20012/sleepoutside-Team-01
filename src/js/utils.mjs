5b0214233e2ea35618f6d92dc61eff6187f83fb1
Guevarachirino <chi23017@byui.edu>
2026-01-15 23:20:52 -0500

    new

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -21,23 +21,3 @@ export function setClick(selector, callback) {
   });
   qs(selector).addEventListener("click", callback);
 }
-
-
-<<<<<<< HEAD
-=======
-export function getParam(param) {
-  const queryString = window.location.search;
-  const urlParams = new URLSearchParams(queryString);
-  const product = urlParams.get(param);
-  return product
-}
-
-export function renderListWithTemplate(template, parentElement, list, position = "afterbegin", clear = false) {
-  const htmlStrings = list.map(template);
-  // if clear is true we need to clear out the contents of the parent.
-  if (clear) {
-    parentElement.innerHTML = "";
-  }
-  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
-}
->>>>>>> 037db383eea224d37ffe98870e76e1e94d7408a0
